3a7dfd3c1aaa0d32207f33537537e5dce86e8428
James Scheiber <jscheiber22@gmail.com>
2023-11-14 15:07:09 -0800

    created an endpoint to receive new project form data, including a cover image. so far it works as expected, next is adding this data to the database

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sqlite3
-from flask import Flask, jsonify
-from flask_cors import CORS
+from flask import Flask, jsonify, request, Response
+from flask_cors import CORS, cross_origin
+import json
 
 app = Flask(__name__)
 CORS(app)
@@ -15,9 +16,23 @@ def get_db_connection():
 def Projects():
     curr = get_db_connection()
     projects = curr.execute('SELECT * FROM Cars').fetchall()
-    print(projects)
+    # print(projects)
     curr.close()
     return jsonify(projects)
+
+@app.route('/api/new_project', methods=['POST'])
+def NewProject():
+    resp = Response("0")
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+
+    print("text")
+    file = request.files.get('file')
+    file.save("pic.jpg")
+    car_data = request.form['car_details']
+    print(file)
+    print(car_data)
+
+    return resp
 
 
 if __name__ == "__main__":
