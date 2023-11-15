375231ea1953f4455f1f29edbdaf780f6df51ebd
James Scheiber <jscheiber22@gmail.com>
2023-11-15 13:41:28 -0800

    fully working add and remove on back end, modification still needs to be added, as well as functions for database tables other than just Cars

--- main.py
+++ main.py
@@ -9,30 +9,64 @@ CORS(app)
 def get_db_connection():
     conn = sqlite3.connect('../db/cars_parts_database.db')
     curr = conn.cursor()
-    return curr
+    return conn, curr
 
 
 @app.route('/api/projects')
 def Projects():
-    curr = get_db_connection()
+    conn, curr = get_db_connection()
     projects = curr.execute('SELECT * FROM Cars').fetchall()
-    # print(projects)
-    curr.close()
+    conn.close()
     return jsonify(projects)
+
 
 @app.route('/api/new_project', methods=['POST'])
 def NewProject():
-    resp = Response("0")
-    resp.headers['Access-Control-Allow-Origin'] = '*'
+    try:
+        # required to appease CORS otherwise it throws a big fit
+        resp = Response("0")
+        resp.headers['Access-Control-Allow-Origin'] = '*'
 
-    print("text")
-    file = request.files.get('file')
-    file.save("pic.jpg")
-    car_data = request.form['car_details']
-    print(file)
-    print(car_data)
+        file = request.files.get('file')
+        if file:
+            file.save("pic.jpg")
+        car_data = json.loads(request.form['car_details'])
 
-    return resp
+        conn, curr = get_db_connection()
+        curr.execute('''INSERT INTO Cars(Make, Model, Year, Misc_Info, img_Location) VALUES ('{}', '{}', {}, '{}', '{}')'''.format(car_data["Make"], car_data["Model"], int(car_data["Year"]), car_data["Description"], ""))
+
+
+    except:
+        resp = Response("-1")
+        raise
+
+    finally:
+        resp.headers['Access-Control-Allow-Origin'] = '*'
+        conn.commit()
+        conn.close()
+        return resp
+
+
+@app.route('/api/del_project/<id>', methods=['POST'])
+def DelProject(id):
+    try:
+        # required to appease CORS otherwise it throws a big fit
+        resp = Response("0")
+        resp.headers['Access-Control-Allow-Origin'] = '*'
+
+        conn, curr = get_db_connection()
+
+        curr.execute('''DELETE FROM Cars WHERE ID={}'''.format(id))
+
+    except:
+        resp = Response("-1")
+        raise
+
+    finally:
+        resp.headers['Access-Control-Allow-Origin'] = '*'
+        conn.commit()
+        conn.close()
+        return resp
 
 
 if __name__ == "__main__":
