c62744753024187caefd527e44f25a7b4b9c9e11
James Scheiber <jscheiber22@gmail.com>
2023-11-18 13:45:40 -0800

    now successfully intakes images and saves them to the sqlite database. also is able to pull them from the database and send them with the other project data to the front end

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import base64
 import sqlite3
-from flask import Flask, jsonify, request, Response
-from flask_cors import CORS, cross_origin
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 import json
 
 app = Flask(__name__)
@@ -12,61 +13,46 @@ def get_db_connection():
     return conn, curr
 
 
-@app.route('/api/projects')
+@app.route('/api/projects', methods=['GET'])
 def Projects():
     conn, curr = get_db_connection()
     projects = curr.execute('SELECT * FROM Cars').fetchall()
     conn.close()
+
     return jsonify(projects)
 
 
-@app.route('/api/new_project', methods=['POST'])
+@app.route('/api/projects', methods=['POST'])
 def NewProject():
-    try:
-        # required to appease CORS otherwise it throws a big fit
-        resp = Response("0")
-        resp.headers['Access-Control-Allow-Origin'] = '*'
+    uploaded_file = request.files.get('file')
+    if uploaded_file:
+        image_data = uploaded_file.read()
+        encoded_image_data = base64.b64encode(image_data).decode('utf-8')
+    car_data = json.loads(request.form['car_details'])
 
-        file = request.files.get('file')
-        if file:
-            file.save("pic.jpg")
-        car_data = json.loads(request.form['car_details'])
+    conn, curr = get_db_connection()
+    curr.execute('''INSERT INTO Cars(Make, Model, Year, Misc_Info, Image) VALUES ('{}', '{}', {}, '{}', '{}')'''.format(car_data["Make"], car_data["Model"], int(car_data["Year"]), car_data["Description"], encoded_image_data))
+    
+    conn.commit()
+    conn.close()
 
-        conn, curr = get_db_connection()
-        curr.execute('''INSERT INTO Cars(Make, Model, Year, Misc_Info, img_Location) VALUES ('{}', '{}', {}, '{}', '{}')'''.format(car_data["Make"], car_data["Model"], int(car_data["Year"]), car_data["Description"], ""))
-
-
-    except:
-        resp = Response("-1")
-        raise
-
-    finally:
-        resp.headers['Access-Control-Allow-Origin'] = '*'
-        conn.commit()
-        conn.close()
-        return resp
+    return "0"
 
 
 @app.route('/api/del_project/<id>', methods=['POST'])
 def DelProject(id):
     try:
-        # required to appease CORS otherwise it throws a big fit
-        resp = Response("0")
-        resp.headers['Access-Control-Allow-Origin'] = '*'
-
         conn, curr = get_db_connection()
-
         curr.execute('''DELETE FROM Cars WHERE ID={}'''.format(id))
 
+        conn.commit()
+        conn.close()
+
     except:
-        resp = Response("-1")
         raise
 
     finally:
-        resp.headers['Access-Control-Allow-Origin'] = '*'
-        conn.commit()
-        conn.close()
-        return resp
+        return "0"
 
 
 if __name__ == "__main__":
